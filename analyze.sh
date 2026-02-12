6d0ecda32f6fe9c1ca5b019a7ffa7bda10ab2bd5
nautomo <nautomo@cs544-nautomo.cs.wisc.edu>
2026-02-11 20:29:21 -0600

    finished part 3 - updated 2.0

--- analyze.sh
+++ analyze.sh
@@ -3,13 +3,8 @@ set -e
 
 ./gen_prompt.sh > /dev/null 2>&1
 
-python3 - <<EOF > calculator/prompt.json
-import json
-print(json.dumps({"system_prompt": open("calculator/prompt.txt").read()}))
-EOF
-
 bash ./google_gemma-3-4b-it-Q6_K.llamafile \
-  --system-prompt-file calculator/prompt.json \
+  -f calculator/prompt.txt \
   --silent-prompt \
   --log-disable \
   --temp 0 \
