9f97504a32530f1ac048e1fc5a34ea7e66e6a2ba
Your Name <you@example.com>
2026-03-11 14:07:15 -0500

    finished part 1

--- app_cli/cache.py
+++ app_cli/cache.py
@@ -1,12 +1,27 @@
 import sys
 import time
 import requests
+import threading
+from collections import deque
+
+lock = threading.Lock()
+cache = {}
+order = deque()
+cache_size = 0
 
 def init_cache(size):
-    pass # TODO: initialize any global data structures as needed
+    # TODO: initialize any global data structures as needed
+    global cache, order, cache_size
+    cache = {}
+    order = deque()
+    cache_size = size
 
 def http_get(url, http_session):
     """Fetch url with retries (up to 5 seconds total wait). Returns (content_bytes, hit)."""
+    with lock:
+        if url in cache:
+            return cache[url], True
+
     wait = 0.1
     total_waited = 0
     while True:
@@ -19,8 +34,22 @@ def http_get(url, http_session):
                 raise
             print(f"RETRY {url} after {wait}s (total waited: {total_waited}s)")
             time.sleep(wait)
-    resp.raise_for_status()
-    return resp.content, False
+
+    if resp.status_code == 404:
+        content = None
+    else:
+        resp.raise_for_status()
+        content = resp.content
+
+    with lock:
+        if url not in cache:
+            if len(cache) >= cache_size:
+               first_in = order.popleft()
+               cache.pop(first_in)
+            cache[url] = content
+            order.append(url)
+
+    return content, False
 
 if __name__ == "__main__":
     init_cache(3)
