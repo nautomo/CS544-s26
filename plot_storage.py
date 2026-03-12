22308376643b18b123cc3d968429b95daa0b83e7
Your Name <you@example.com>
2026-03-11 19:49:33 -0500

    finished part 2

---
+++ plot_storage.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import csv
+
+formats = []
+times = []
+
+with open("storage_times.csv") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        formats.append(row["format"])
+        times.append(float(row["time_seconds"]))
+
+plt.bar(formats, times)
+plt.ylabel("Seconds")
+plt.title("Storage Format Load Time")
+plt.savefig("storage.svg")
+plt.show()
