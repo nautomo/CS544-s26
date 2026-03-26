8c23f02ad7771f0e167c69c18bfc9967f828731e
Your Name <you@example.com>
2026-03-25 22:53:34 -0500

    finished part 1

--- server.py
+++ server.py
@@ -22,7 +22,39 @@ class LenderService(lender_pb2_grpc.LenderServicer):
             try:
                 engine = create_engine(conn_str)
                 with engine.connect() as conn:
-                    return lender_pb2.DbToHdfsResp(error="connected")
+                    query = """
+                        SELECT l.*
+                        FROM loans l
+                        INNER JOIN loan_types t
+                            ON l.loan_type_id = t.id
+                        WHERE t.loan_type_name = 'Conventional'
+                        LIMIT 10
+                    """
+                    df = pd.read_sql(query, conn)
+                    row_count = len(df)
+
+                    os.environ["CLASSPATH"] = subprocess.check_output(
+                        ["hadoop", "classpath", "--glob"], text=True
+                    ).strip()
+
+                    fs = pa.fs.HadoopFileSystem(
+                        host="nn",                     # IMPORTANT: service name
+                        port=9000,
+                        user="root",
+                        replication=2,                 # required
+                        default_block_size=1024 * 1024 # 1 MB
+                    )
+
+                    table = pa.Table.from_pandas(df)
+
+                    pq.write_table(
+                        table,
+                        "hdfs://nn:9000/hdma-wi-2021.parquet",
+                        filesystem=fs,
+                        compression="snappy"
+                    )
+
+                    return lender_pb2.DbToHdfsResp(error="", row_count=row_count)
             except OperationalError as e:
                 print(f"MySQL not ready: {e}", file=sys.stderr)
                 if attempt < 4:
@@ -41,9 +73,10 @@ class LenderService(lender_pb2_grpc.LenderServicer):
         return lender_pb2.CalcAvgLoanResp(error="not implemented")
 
 print("start server")
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=8))
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=8), options=[("grpc.so_reuseport", 0)])
 lender_pb2_grpc.add_LenderServicer_to_server(LenderService(), server)
-server.add_insecure_port("0.0.0.0:5000")
+port = server.add_insecure_port("[::]:5000")
+print(f"Bound to port: {port}", flush=True)
 server.start()
 print("gRPC server listening on port 5000...", flush=True)
 server.wait_for_termination()
