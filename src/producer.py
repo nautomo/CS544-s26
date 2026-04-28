ad0b0a44c012ff4d8fb46e829cad5158b744b545
nautomo <nautomo@cs544-nautomo.cs.wisc.edu>
2026-04-28 14:02:57 -0500

    finished part 1

--- src/producer.py
+++ src/producer.py
@@ -9,7 +9,7 @@ import report_pb2
 PROJECT = os.environ["PROJECT"]
 MYSQL_HOST = f"{PROJECT}-mysql-1"
 
-DB_URI = f"mysql+pymysql://root:abc@{MYSQL_HOST}:3306/CS544"
+DB_URI = f"mysql+mysqlconnector://root:abc@{MYSQL_HOST}:3306/CS544"
 
 TOPIC = "stock_prices"
 BROKER = "localhost:9092"
@@ -24,14 +24,20 @@ def init_topic():
     except UnknownTopicOrPartitionError:
         pass
 
-    topic = NewTopic(name=TOPIC, num_partitions=4, replication_factor=1)
-    admin.create_topics([topic])
-
+    while True:
+        try:
+            admin.create_topics([
+                NewTopic(name=TOPIC, num_partitions=4, replication_factor=1)
+            ])
+            break
+        except Exception:
+            time.sleep(1)
 
 def main():
+    print("PRODUCER STARTED", flush=True)
     init_topic()
 
-    engine = create_engine(DB_URI)
+    engine = create_engine(DB_URI, pool_pre_ping=True)
 
     producer = KafkaProducer(
         bootstrap_servers=BROKER,
@@ -44,17 +50,19 @@ def main():
     while True:
         with engine.connect() as conn:
             result = conn.execute(
-                text("SELECT id, ticker, date, price FROM stock_prices WHERE id > :id ORDER BY id ASC LIMIT 1000"),
+                text("SELECT id, ticker, date, price FROM stock_prices WHERE id > :id ORDER BY id ASC"),
                 {"id": last_id}
             )
 
             rows = result.fetchall()
 
         if not rows:
-            time.sleep(1)
+            print("no rows")
+            time.sleep(0.01)
             continue
 
         for row in rows:
+            print("entered loop")
             last_id = row.id
 
             report = report_pb2.Report()
@@ -62,13 +70,17 @@ def main():
             report.price = float(row.price)
             report.ticker = row.ticker
 
+            print(f"SENDING {row.ticker} {row.price}", flush=True)
             producer.send(
                 TOPIC,
                 key=row.ticker.encode(),
                 value=report.SerializeToString()
             )
+            print("producer sent?")
 
         producer.flush()
+        print("producer flushed?")
+        time.sleep(0.01)
 
 
 if __name__ == "__main__":
